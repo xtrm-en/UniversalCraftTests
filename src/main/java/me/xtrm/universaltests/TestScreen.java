676b70c8663f7d33227da3681437ca4091e594ce
xTrM_ <xtrmyt24@gmail.com>
2021-05-15 22:59:43 +0200

    Initial commit

---
+++ src/main/java/me/xtrm/universaltests/TestScreen.java
@@ -0,0 +1,10 @@
+package me.xtrm.universaltests;
+
+import club.sk1er.mods.core.universal.UScreen;
+
+public class TestScreen extends UScreen {
+    @Override
+    public void initGui() {
+        super.initGui();
+    }
+}
